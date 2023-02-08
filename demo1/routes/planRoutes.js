32edf9dffba3a3322609be056fefb28ccd793b0c
qianwei-yin <qianweiyin22@gmail.com>
2023-02-07 18:31:03 -0800

    use jsonschema package for validation

--- demo1/routes/planRoutes.js
+++ demo1/routes/planRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
-const { test, getPlan, createPlan, deletePlan } = require('../controllers/planController');
+const { getPlan, createPlan, deletePlan } = require('../controllers/planController');
 
 const router = express.Router();
 
-router.route('/').get(test).post(createPlan);
+router.route('/').post(createPlan);
 router.route('/:id').get(getPlan).delete(deletePlan);
 
 module.exports = router;
